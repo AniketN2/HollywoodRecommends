7d0e5e2fafc0a77b0eed9f78facf5bfc2a1c952c
AniketN2 <121167260+AniketN2@users.noreply.github.com>
2024-11-15 19:13:53 +0530

    hj

--- main.py
+++ main.py
@@ -187,6 +187,6 @@ def recommend():
 if __name__ == '__main__':
     CORS(app)  
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True,host="0.0.0.0", port=port)
 
 
